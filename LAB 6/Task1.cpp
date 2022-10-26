4fae8da816be94a0d201c579d03c1a5e50bba42b
Imran Javed <96229355+ImranJaved1073@users.noreply.github.com>
2022-10-26 16:33:26 +0500

    Lab 6

---
+++ LAB 6/Task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float b, x, y;
+    cin >> a;
+    cin >> b;
+    x = a * b;
+    y = a / b;
+    cout << "Product : " << x << endl;
+    cout << "Division: " << y;
+    return 0;
+}
---
+++ LAB 6/Task2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c, d;
+	cin >> a;
+	cin >> b;
+	c = a + b;
+	d = a * b;
+	cout << "Sum : " << c << endl;
+	cout << "Product: " << d;
+	return 0;
+}
