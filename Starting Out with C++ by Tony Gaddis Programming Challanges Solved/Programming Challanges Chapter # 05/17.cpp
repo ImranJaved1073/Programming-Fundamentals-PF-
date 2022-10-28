4487964a15181c8a8fa396135edac01be70d5d59
Imran Javed <96229355+ImranJaved1073@users.noreply.github.com>
2022-10-28 19:17:37 +0500

    Programming Challenge 17

---
+++ Starting Out with C++ by Tony Gaddis Programming Challanges Solved/Programming Challanges Chapter # 05/17.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int sales[5], sale = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Enter todays sales for store " << i + 1 << ":";
+        cin >> sale;
+        sales[i] = sale / 100;
+    }
+    cout << "SALES BAR CHART\n"
+        << "(Each * = $100)";
+    for (int c = 0; c < 5; c++)
+    {
+        cout << "Store" << c + 1 << ":";
+        for (int j = 0; j < sales[c]; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    const int MIN_NUMBER = 0,
+        NUM_OF_STORES = 5,
+        HUNDRED = 100;
+
+    int store_1,
+        store_2,
+        store_3,
+        store_4,
+        store_5,
+        store_1_100s,
+        store_2_100s,
+        store_3_100s,
+        store_4_100s,
+        store_5_100s;
+
+    cout << "\nEnter today's sales for store 1: ";
+    cin >> store_1;
+    store_1_100s = store_1 / HUNDRED; // 200/100 = 2
+
+    cout << "Enter today's sales for store 2: ";
+    cin >> store_2;
+    store_2_100s = store_2 / HUNDRED; // 300/100 = 3
+
+    cout << "Enter today's sales for store 3: ";
+    cin >> store_3;
+    store_3_100s = store_3 / HUNDRED;
+
+    cout << "Enter today's sales for store 4: ";
+    cin >> store_4;
+    store_4_100s = store_4 / HUNDRED;
+
+    cout << "Enter today's sales for store 5: ";
+    cin >> store_5;
+    store_5_100s = store_5 / HUNDRED;
+
+    cout << "\nSALES BAR CHART" << endl;
+    cout << "(Each * = $100)" << endl;
+    cout << "Store 1: ";
+    for (int i = MIN_NUMBER; i < store_1_100s; i++)
+        cout << '*';
+
+    cout << endl;
+
+    cout << "Store 2: ";
+    for (int i = MIN_NUMBER; i < store_2_100s; i++)
+        cout << '*';
+
+    cout << endl;
+
+    cout << "Store 3: ";
+    for (int i = MIN_NUMBER; i < store_3_100s; i++)
+        cout << '*';
+
+    cout << endl;
+
+    cout << "Store 4: ";
+    for (int i = MIN_NUMBER; i < store_4_100s; i++)
+        cout << '*';
+
+    cout << endl;
+
+    cout << "Store 5: ";
+    for (int i = MIN_NUMBER; i < store_5_100s; i++)
+        cout << '*';
+
+    cout << '\n' << endl;
+
+    return 0;
+}*/
